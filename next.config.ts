24f2f08aff4b472c0db14d202c979b52d0d1573f
Vilhelm Bergqvist <vilhelm.bergqvist@gmail.com>
2025-10-11 14:54:16 +0200

    Updated deps

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next"
 import "./src/env.js"
 
 const config: NextConfig = {
+  typedRoutes: true,
   eslint: {
     dirs: ["src"],
     ignoreDuringBuilds: true,
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,15 +1,14 @@
 import { PrismaClient } from "@/generated/prisma/client"
-// import { Pool } from "pg"
+import { Pool } from "pg"
 import { env } from "@/env"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 const createPrismaClient = () => {
-  // const pool = new Pool({
-  //   connectionString: env.DATABASE_URL,
-  // })
-  const adapter = new PrismaPg({
+  const pool = new Pool({
     connectionString: env.DATABASE_URL,
-  }) //Does not work with schemas apparently.
+  })
+
+  const adapter = new PrismaPg(pool) //Does not work with schemas apparently.
   // Seems to be no workaround in postgres, so you have to use the public schema to use a adapter.
   return new PrismaClient({
     adapter,
