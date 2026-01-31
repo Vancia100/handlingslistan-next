f01a899d4d9bce7612ad532c1d437d1b804af799
Vilhelm Bergqvist <vilhelm.bergqvist@gmail.com>
2026-01-31 17:12:57 +0100

    First Caddy test

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,47 +3,46 @@ import { auth } from "@hndl/auth/server"
 import { toNodeHandler } from "better-auth/node"
 import * as trpcExpress from "@trpc/server/adapters/express"
 import { appRouter, createContext } from "@hndl/api"
-import cors from "cors"
 import httpProxy from "http-proxy"
 import http from "http"
+
+const PROD = process.env.ENVIRONMENT == "PRODUCTION"
+
 const app = express()
 const port = process.env.PORT || 3001
 
 const server = http.createServer(app)
 
-server.on("upgrade", (req, socket, head) => {
-  console.log("test")
-  proxy.ws(req, socket, head, {
-    target: "ws://localhost:3000",
-  })
-})
-const proxy = httpProxy.createProxyServer({
-  ws: true,
-})
-//Make sure that the nextJS backend can be used
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
-)
+const apiRouter = express.Router()
 // Auth
-app.all("/api/auth/*splat", toNodeHandler(auth))
+apiRouter.all("/auth/*splat", toNodeHandler(auth))
 
 //trpc
-app.use(
-  "/api/trpc",
+apiRouter.use(
+  "/trpc",
   trpcExpress.createExpressMiddleware({
     router: appRouter,
     createContext,
   }),
 )
+app.use(PROD ? "/" : "/api", apiRouter)
 // Do not use this in production code.
-app.use("/", (req, res) => {
-  proxy.web(req, res, {
-    target: "http://localhost:3000",
+if (!PROD) {
+  const proxy = httpProxy.createProxyServer({
+    ws: true,
   })
-})
+  server.on("upgrade", (req, socket, head) => {
+    console.log("test")
+    proxy.ws(req, socket, head, {
+      target: "ws://localhost:3000",
+    })
+  })
+  app.use("/", (req, res) => {
+    proxy.web(req, res, {
+      target: "http://localhost:3000",
+    })
+  })
+}
 
 server.listen(port, () => {
   console.log("Started server on Port", port)
